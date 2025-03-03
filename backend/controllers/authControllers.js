8460b6fcdd198769a3eb7920779eb9d04b571009
AstroWalker24 <nadeemshaik2003@gmail.com>
2025-03-03 09:34:40 +0530

    feat: add basic authentication handlers (signup, signin, signout)

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,11 @@
+export const handleSignup = (req, res) => {
+    
+}
+
+export const handleSignin = (req, res) => {
+    res.send('Signin route');
+}
+
+export const handleSignout = (req,res) => {
+    res.send('Signout route');
+}
